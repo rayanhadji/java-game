e1c67a37b3a0a42ba5c6ba2e41457b47fbb987bf
Youcef-dev88 <kaoudjtyoucefmadjid@gmail.com>
2025-12-18 12:03:35 +0100

    Refactor Archer class to include target parameter
    knowing enrmies Hp each time

--- src/Units/Archer.java
+++ src/Units/Archer.java
@@ -3,15 +3,19 @@ package Units;
 import Enemies.Enemy;
 
 public class Archer extends Unit {
-    public Archer() {
+    Enemy target;
+    public Archer(int health,int attack,int defense, int range, int cost,Enemy target) {
         super(80, 25, 5, 3, 60);
+        this.target = target;
     }
 
     @Override
     public void attack(Enemy target) {
-        int dealt = target.takeDamage(this.attack);
+        int dealt = target.takeDamage(5);
         System.out.println("Archer attacked for " + dealt + " damage.");
     }
-
+public int targethp() {
+        return target.health;
+    }
 
 }
