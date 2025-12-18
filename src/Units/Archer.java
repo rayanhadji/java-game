f4347c98e30b7c6150fef4a4d39b1717c05d1757
Youcef-dev88 <kaoudjtyoucefmadjid@gmail.com>
2025-12-18 12:11:10 +0100

    Correct Archer constructor syntax
    Fix constructor definition for Archer class.

--- src/Units/Archer.java
+++ src/Units/Archer.java
@@ -3,6 +3,7 @@ package Units;
 import Enemies.Enemy;
 
 public class Archer extends Unit {
+    public Archer{
     Enemy target;
     public Archer(int health,int attack,int defense, int range, int cost,Enemy target) {
         super(80, 25, 5, 3, 60);
