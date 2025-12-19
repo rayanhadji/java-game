3d1806aa9ad8a5cf6bb1a01a5a30c61fcc52aeea
rayanhadji <rayanhadji.fr@gmail.com>
2025-12-19 13:19:20 -0800

    fixed units

--- src/Units/Archer.java
+++ src/Units/Archer.java
@@ -5,15 +5,13 @@ import java.util.Map;
 
 public class Archer extends Unit {
     public Archer() {
-        super("Archer", 25, 15, 3, 5, 2, createCost());
+        super("Soldier", 25, 15, 5, 5, 2, createCost());
     }
 
-    @Override
-    public void attack(Enemy target) {
-        int dealt = target.takeDamage(5);
-        System.out.println("Archer attacked for " + dealt + " damage.");
-    }
-public int targethp() {
-        return target.health;
+    private static Map<String, Integer> createCost() {
+        Map<String, Integer> cost = new HashMap<>();
+        cost.put("Food", 15);
+        cost.put("Gold", 10);
+        return cost;
     }
 }
